dc55b341da5c5ffd2a2efee9f546ad19562cc6ce
IDarKay <boyeralice0@gmail.com>
2019-11-13 15:17:27 +0100

    :tada: initial commit

---
+++ api/src/main/java/fr/idarkay/minetasia/core/api/MinetasiaCoreApi.java
@@ -0,0 +1,16 @@
+package fr.idarkay.minetasia.core.api;
+
+import fr.idarkay.minetasia.normes.MinetasiaPlugin;
+
+/**
+ * File <b>MinetasiaCoreApi</b> located on fr.idarkay.minetasia.core.api
+ * MinetasiaCoreApi is a part of MinetasiaCore.
+ * <p>
+ * Copyright (c) 2019 MinetasiaCore.
+ * <p>
+ *
+ * @author alice. B. (IDarKay),
+ * Created the 13/11/2019 at 15:05
+ */
+public class MinetasiaCoreApi extends MinetasiaPlugin {
+}
---
+++ common/src/main/java/fr/idarkay/mintasia/core/common/MinetasiaCore.java
@@ -0,0 +1,16 @@
+package fr.idarkay.mintasia.core.common;
+
+import fr.idarkay.minetasia.core.api.MinetasiaCoreApi;
+
+/**
+ * File <b>MinetasiaCore</b> located on fr.idarkay.mintasia.core.common
+ * MinetasiaCore is a part of MinetasiaCore.
+ * <p>
+ * Copyright (c) 2019 MinetasiaCore.
+ * <p>
+ *
+ * @author alice. B. (IDarKay),
+ * Created the 13/11/2019 at 15:06
+ */
+public class MinetasiaCore extends MinetasiaCoreApi {
+}
