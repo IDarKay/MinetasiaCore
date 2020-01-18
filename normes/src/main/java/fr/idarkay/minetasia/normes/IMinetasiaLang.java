7073616d3d004eeeff60bde41cef8e9438a1e00f
IDarKay <boyeralice0@gmail.com>
2020-01-18 19:30:58 +0100

    :truck: move normes to this project

---
+++ normes/src/main/java/fr/idarkay/minetasia/normes/IMinetasiaLang.java
@@ -0,0 +1,20 @@
+package fr.idarkay.minetasia.normes;
+
+/**
+ * File <b>IMinetasiaLang</b> located on fr.idarkay.minetasia.normes
+ * IMinetasiaLang is a part of Normes.
+ * <p>
+ * Copyright (c) 2019 Normes.
+ * <p>
+ *
+ * @author alice. B. (IDarKay),
+ * Created the 13/11/2019 at 15:45
+ * @since 1.0
+ *
+ * implements your lang enum with this for get the standard methods
+ */
+public interface IMinetasiaLang {
+
+    String get(String lang, Object... args);
+
+}
