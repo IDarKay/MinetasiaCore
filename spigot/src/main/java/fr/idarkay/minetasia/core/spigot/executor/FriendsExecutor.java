ddca52dad869f615b08398906b77d668b6610d9d
IDarKay <aloisboyer58@gmail.com>
2020-03-04 23:34:37 +0100

    :truck: remove maj to package

--- spigot/src/main/java/fr/idarkay/minetasia/core/spigot/executor/FriendsExecutor.java
+++ spigot/src/main/java/fr/idarkay/minetasia/core/spigot/executor/FriendsExecutor.java
@@ -1,4 +1,4 @@
-package fr.idarkay.minetasia.core.spigot.Executor;
+package fr.idarkay.minetasia.core.spigot.executor;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
