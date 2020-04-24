f849fa36472907525bc128437fcd3543825be61d
IDarKay <boyeralice0@gmail.com>
2020-04-24 02:41:39 +0200

    featurs

---
+++ api/src/main/java/fr/idarkay/minetasia/core/api/event/PlayerJoinEvent.java
@@ -0,0 +1,60 @@
+package fr.idarkay.minetasia.core.api.event;
+
+import fr.idarkay.minetasia.core.api.utils.MinetasiaPlayer;
+import org.apache.commons.lang.Validate;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * File <b>PlayerJoinEvent</b> located on fr.idarkay.minetasia.core.api.event
+ * PlayerJoinEvent is a part of MinetasiaCore.
+ * <p>
+ * Copyright (c) 2020 MinetasiaCore.
+ * <p>
+ *
+ * @author alice. B. (IDarKay),
+ * Created the 23/04/2020 at 23:20
+ */
+public class PlayerJoinEvent extends Event
+{
+
+    private static HandlerList handlerList = new HandlerList();
+
+    @NotNull
+    private final Player bukkitPlayer;
+    @NotNull
+    private final MinetasiaPlayer minetasiaPlayer;
+
+    public PlayerJoinEvent(@NotNull final MinetasiaPlayer minetasiaPlayer,  @NotNull final Player bukkitPlayer)
+    {
+        super(true);
+        this.minetasiaPlayer = minetasiaPlayer;
+        Validate.notNull(bukkitPlayer);
+        this.bukkitPlayer = bukkitPlayer;
+    }
+
+    @NotNull
+    public Player getPlayer()
+    {
+        return bukkitPlayer;
+    }
+
+    @NotNull
+    public MinetasiaPlayer getMinetasiaPlayer()
+    {
+        return minetasiaPlayer;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return handlerList;
+    }
+
+    public static HandlerList getHandlerList()
+    {
+        return handlerList;
+    }
+
+}
