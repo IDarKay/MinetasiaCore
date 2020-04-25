904e14835bae6f443a3fc800c63017e6935c9fc2
IDarKay <aloisboyer58@gmail.com>
2020-04-26 01:56:37 +0200

    inv sync

---
+++ spigot/src/main/java/fr/idarkay/minetasia/core/spigot/utils/InventorySyncType.java
@@ -0,0 +1,16 @@
+package fr.idarkay.minetasia.core.spigot.utils;
+
+/**
+ * File <b>InventorySyncType</b> located on fr.idarkay.minetasia.core.api
+ * InventorySyncType is a part of MinetasiaCore.
+ * <p>
+ * Copyright (c) 2020 MinetasiaCore.
+ * <p>
+ *
+ * @author Alois. B. (IDarKay),
+ * Created the 25/04/2020 at 19:12
+ */
+public enum InventorySyncType
+{
+    NONE, BDD, DAT
+}
